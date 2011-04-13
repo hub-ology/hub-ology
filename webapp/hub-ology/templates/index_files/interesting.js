b8966fc7d9c376ae8421b8df518e7f144ad72ef9
Brian S. Corbin <corbinbs@gmail.com>
2011-04-13 01:12:14 -0400

    rough draft of a main index page

---
+++ webapp/hub-ology/templates/index_files/interesting.js
@@ -0,0 +1,123 @@
+
+// HTML strings for each image
+var gaSnippets = new Array();
+
+gaSnippets.push("<img id=bytesperpage class=chart src='http://chart.apis.google.com/chart?chs=400x225&cht=p&chco=007099&chd=t:34,450,123,27,84,25&chds=25,450&chl=HTML+-+34+kB%7CImages+-+450+kB%7CScripts+-+123+kB%7CStylesheets+-+27+kB%7CFlash+-+84+kB%7COther+-+25+kB&chma=|5&chtt=Average+Bytes+per+Page+by+Content+Type'>");
+gaSnippets.push("<img id=responsesizes class=chart src='http://chart.apis.google.com/chart?chxl=1:|Flash%7CCSS%7CJS%7CHTML%7CPNG%7CJPEG%7CGIF&chdlp=b&chdl=average response size (kB)&chxtc=0,6&chxs=0,676767,11.5,0,l|1,676767,11.5,1,lt,67676700&chxr=1,0,160|0,0,68&chxt=x,y&chbh=22&chs=640x260&cht=bhg&chco=3B356A&chds=0,68&chd=t:3,14,8,6,10,7,58&chm=N**+kB,676767,0,,12,,:4:&chma=|0,5&chtt=Avg+Individual+Resource+Response+Size'>");
+gaSnippets.push("<img id=popularjslib class=chart src='http://chart.apis.google.com/chart?chxp=0,20,40,60,80,100&chxl=0:|20%|40%|60%|80%|100%|1:|ShareThis%7CTwitter%7CFacebook%7CAddThis%7CQuantcast%7CGoogle+Analytics%7CDojo%7CYUI%7CjQuery&chdlp=b&chdl=sites using the JS library&chxtc=0,6&chxs=0,676767,11.5,0,l|1,676767,11.5,1,lt,67676700&chxr=1,0,160|0,0,100&chxt=x,y&chbh=22&chs=640x370&cht=bhg&chco=3399CC&chds=0,100&chd=t:43,2,1,62,14,4,17,3,1&chm=N**%,676767,0,,12,,:4:&chma=|0,5&chtt=Popular+JavaScript+Libraries'>");
+gaSnippets.push("<img id=popularjs class=chart src='http://chart.apis.google.com/chart?chxp=0,20,40,60,80,100&chxl=0:|20%|40%|60%|80%|100%|1:|http%3A%2F%2Fpagead2.googlesyndication.com%2Fpagead%2Fshow_ads.js%7Chttp%3A%2F%2Fgoogleads.g.doubleclick.net%2Fpagead%2Ftest_domain.js%7Chttp%3A%2F%2Fpagead2.googlesyndication.com%2Fpagead%2Frender_ads.js%7Chttp%3A%2F%2Fedge.quantserve.com%2Fquant.js%7Chttp%3A%2F%2Fwww.google-analytics.com%2Fga.js&chdlp=b&chdl=sites using the script&chxtc=0,6&chxs=0,676767,11.5,0,l|1,676767,11.5,1,lt,67676700&chxr=1,0,160|0,0,100&chxt=x,y&chbh=22&chs=640x220&cht=bhg&chco=1D7D61&chds=0,100&chd=t:53,14,10,10,10&chm=N**%,676767,0,,12,,:4:&chma=|0,5&chtt=Most+Popular+Scripts'>");
+gaSnippets.push("<img id=googlelibs class=chart src='http://chart.apis.google.com/chart?chs=400x225&cht=p&chco=7777CC&chd=t:88,12&chl=no+88%25%7Cyes+12%25&chma=|5&chtt=Pages+Using+Google+Libraries+API'>");
+gaSnippets.push("<img id=mostjs class=chart src='http://chart.apis.google.com/chart?chxl=1:|average%7Chttp%3A%2F%2Fwww.chryslerfinancial.com%2F%7Chttp%3A%2F%2Fwww.nissan.co.jp%2F%7Chttp%3A%2F%2Fwww.live365.com%2F%7Chttp%3A%2F%2Fwww.craktraffic.com%2F%7Chttp%3A%2F%2Fwww.unicaondemand.com%2F&chdlp=b&chdl=size of all scripts (kB)&chxtc=0,6&chxs=0,676767,11.5,0,l|1,676767,11.5,1,lt,67676700&chxr=1,0,160|0,0,4021&chxt=x,y&chbh=22&chs=640x260&cht=bhg&chco=B4B418&chds=0,4021&chd=t:3921,2810,1470,1381,1341,123&chm=N**+kB,676767,0,,12,,:4:&chma=|0,5&chtt=Pages+with+the+Most+JavaScript'>");
+gaSnippets.push("<img id=mostcss class=chart src='http://chart.apis.google.com/chart?chxl=1:|average%7Chttp%3A%2F%2Fwww.leroymerlin.fr%2F%7Chttp%3A%2F%2Fwww.surveynetwork.com%2F%7Chttp%3A%2F%2Fwww.kongregate.com%2F%7Chttp%3A%2F%2Fwww.prenhall.com%2F%7Chttp%3A%2F%2Fwww.pearsonhighered.com%2F&chdlp=b&chdl=size of all stylesheets (kB)&chxtc=0,6&chxs=0,676767,11.5,0,l|1,676767,11.5,1,lt,67676700&chxr=1,0,160|0,0,3789&chxt=x,y&chbh=22&chs=640x260&cht=bhg&chco=CF557B&chds=0,3789&chd=t:3689,3689,1060,901,523,27&chm=N**+kB,676767,0,,12,,:4:&chma=|0,5&chtt=Pages+with+the+Most+CSS'>");
+gaSnippets.push("<img id=flash class=chart src='http://chart.apis.google.com/chart?chs=400x225&cht=p&chco=AA0033&chd=t:53,47&chl=No+Flash+53%25%7CFlash+47%25&chma=|5&chtt=Pages+Using+Flash'>");
+gaSnippets.push("<img id=mostflash class=chart src='http://chart.apis.google.com/chart?chxl=1:|average%7Chttp%3A%2F%2Fwww.hftogo.com%2F%7Chttp%3A%2F%2Fwww.manager.co.th%2F%7Chttp%3A%2F%2Fwww.xat.com%2F%7Chttp%3A%2F%2Fwww.mymodernmet.com%2F%7Chttp%3A%2F%2Fwww.afpbb.com%2F&chxtc=0,6&chxs=0,676767,11.5,0,l|1,676767,11.5,1,lt,67676700&chxr=1,0,160|0,0,84&chxt=x,y&chbh=22&chs=640x260&cht=bhg&chco=AA0033&chds=0,84&chd=t:74,36,34,33,32,1.4354&chm=N**,676767,0,,12,,:4:&chma=|0,5&chtt=Pages+with+the+Most+Flash+Files'>");
+gaSnippets.push("<img id=imageformats class=chart src='http://chart.apis.google.com/chart?chs=400x225&cht=p&chco=E94E19&chd=t:38,43,18&chl=GIF+38%25%7CJPEG+43%25%7CPNG+18%25&chma=|5&chtt=Image+Formats'>");
+gaSnippets.push("<img id=errors class=chart src='http://chart.apis.google.com/chart?chs=400x225&cht=p&chco=B09542&chd=t:72,28&chl=No+Errors+72%25%7CErrors+28%25&chma=|5&chtt=Pages+with+Errors+%284xx%2C+5xx%29'>");
+gaSnippets.push("<img id=redirects class=chart src='http://chart.apis.google.com/chart?chs=400x225&cht=p&chco=008000&chd=t:39,61&chl=No+Redirects+39%25%7CRedirects+61%25&chma=|5&chtt=Pages+with+Redirects+%283xx%29'>");
+gaSnippets.push("<img id=onLoad class=chart src='http://chart.apis.google.com/chart?chxl=1:|Total+Xfer+Size%7CImage+Xfer+Size%7CTotal+Reqs%7CImage+Reqs%7CJS+Reqs&chxr=0,0,1&chxs=1,676767,11.5,0,lt,67676700&chxtc=1,5&chxt=y,x&chbh=60,30,30&chs=500x225&cht=bvg&chco=80C65A&chds=0,1&chd=t:0.77,0.68,0.68,0.64,0.42&chm=N,676767,0,,12,,::4&chtt=Highest+Correlation+to+Load+Time'>");
+gaSnippets.push("<img id=renderStart class=chart src='http://chart.apis.google.com/chart?chxl=1:|JS+Xfer+Size%7CTotal+Reqs%7CCSS+Xfer+Size%7CTotal+Xfer+Size%7CCSS+Reqs&chxr=0,0,1&chxs=1,676767,11.5,0,lt,67676700&chxtc=1,5&chxt=y,x&chbh=60,30,30&chs=500x225&cht=bvg&chco=7777CC&chds=0,1&chd=t:0.32,0.25,0.24,0.23,0.22&chm=N,676767,0,,12,,::4&chtt=Highest+Correlation+to+Render+Time'>");
+
+
+
+// The DOM element that is created from each snippet.
+var gaSnippetElems = new Array();
+var curSnippet;
+
+function showSnippet(parentId, bPrev) {
+	var parent = document.getElementById(parentId);
+	if ( ! parent ) {
+		return;
+	}
+
+	var iSnippet = Math.floor(gaSnippets.length * Math.random());
+	if ( curSnippet ) {
+		iSnippet = parseInt(curSnippet.id)
+		//fade(curSnippet, true);
+		curSnippet.style.display = 'none';
+	}
+
+	iSnippet = ( bPrev ? iSnippet-1 : iSnippet+1 );
+	if ( iSnippet >= gaSnippets.length ) {
+		iSnippet = 0;
+	}
+	else if ( iSnippet < 0 ) {
+		iSnippet = gaSnippets.length - 1;
+	}
+
+	var newSnippet = gaSnippetElems[iSnippet];
+	if ( "undefined" === typeof(newSnippet) ) {
+		newSnippet = document.createElement('div');
+		newSnippet.id = iSnippet;
+		gaSnippetElems[iSnippet] = newSnippet;
+		newSnippet.innerHTML = "<a class=image-link href='interesting.php'>" + gaSnippets[iSnippet] + "</a>";
+		var aPosition = findPos(parent);
+		newSnippet.style.left = aPosition[0] + "px";
+		newSnippet.style.top = aPosition[1] + "px";
+		parent.appendChild(newSnippet);
+	}
+	else {
+		newSnippet.style.display = "block";
+	}
+
+	curSnippet = newSnippet;
+	fade(newSnippet);
+}
+
+
+function insertNav(parentId) {
+	var arrow = document.getElementById('leftarrow');
+	if ( arrow ) {
+		arrow.innerHTML = "<a class='image-link' href='javascript:showSnippet(\"" + parentId + "\", 1)'><img src='images/tri-lft-t-14x28.gif' width=14 height=28 border=0></a>";
+	}
+
+	arrow = document.getElementById('rightarrow');
+	if ( arrow ) {
+		arrow.innerHTML = "<a class='image-link' href='javascript:showSnippet(\"" + parentId + "\")'><img src='images/tri-rt-t-14x28.gif' width=14 height=28 border=0></a>";
+	}
+}
+
+
+// opacity is a number 0-100 inclusive
+function fade(idOrElem, bOut) {
+	var elem = idOrElem;
+	if ( "string" === typeof(idOrElem) || "number" === typeof(idOrElem) ) {
+		elem = document.getElementById(idOrElem);
+	}
+	if ( ! elem ) {
+		return;
+	}
+
+	opacity = ( elem.style.opacity ? parseInt(elem.style.opacity * 100) : ( bOut ? 100 : 0 ) );
+	opacity = ( bOut ? opacity - 10 : opacity + 10 );
+	opacity = ( 100 < opacity ? 100 : ( 0 > opacity ? 0 : opacity ) );
+
+	elem.style.opacity = opacity/100;
+	elem.style.filter = "alpha(opacity = " + opacity + ")";
+
+	if ( (bOut && 0 < opacity ) || ( !bOut && 100 > opacity) ) {
+		setTimeout(function() { fade(elem, bOut); }, 50);
+	}
+	else if ( bOut ) {
+		elem.style.display = "none";
+	}
+}
+
+
+// from http://www.quirksmode.org/js/findpos.html
+function findPos(obj) {
+	var curleft = curtop = 0;
+	if (obj.offsetParent) {
+		do {
+			curleft += obj.offsetLeft;
+			curtop += obj.offsetTop;
+		} while (obj = obj.offsetParent);
+	}
+	return [curleft,curtop];
+}
+
+
+function dprint(msg) {
+	if ( "undefined" != typeof(console) ) {
+		console.log(msg);
+	}
+}
